b82a7110bf50a183bdb3dbbb1d741b7b91c1108b
Wilkinson, Jacob R <jacob.r.wilkinson@boeing.com>
2025-12-10 16:12:28 +1000

    Day 10 Problem solutions Part 1

---
+++ 2025/Day 10.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+2025 Day 10 Problems
+10/12/2025 1:30 pm
+@author: Jacob Wilkinson
+"""
+__author__ = "Jacob Wilkinson"
+__email__ = "jacob.r.wilkinson45@gmail.com"
+__date__ = "10/12/2025 1:30 pm"
+__version__ = "1.0"
+__copyright__ = __author__
+
+# Get data
+with open("inputs/d10_input_ref.txt") as file:
+    for line in file.readlines():
+        line = line.strip()
+
